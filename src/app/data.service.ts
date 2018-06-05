dedb918edf1c45e51e4ef824f986e275a64d6561
Jitesh Singh <jitesh.singh@meikaan.in>
2018-06-05 13:36:13 +0530

    subscribed the data service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -3,11 +3,16 @@ import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class DataService {
 
-  constructor(private _http:Http) { }
-  
+  result:any;
+
+  constructor(private _http: Http) { }
+
+  getUsers() {
+    return this._http.get("/api/users")
+   	.map(result => this.result = result.json().data);
+  }
+
 }
